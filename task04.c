08a200a0351036990c787526970f509d6ff0cdde
e2358203-spec <e2358203@soka-u.jp>
2025-11-21 18:15:18 +0900

    Update task04.c

--- task04.c
+++ task04.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include "header.h"
 
+extern MEM data[USERNUM];
+extern void output(int, int, int);
+
 void task04(int term_num, NMEM *nmem)
 {
-    // 指定のメッセージを表示 (先頭にスペースがあることに注意)
-    printf(" \n回線が塞がってます！\n");
-
-    /* 発信側にビジートーンを送出 */
+    printf("\n 発信が許可されていません！\n");
     output(connect, term_num, BUSYTONE);
 
-    /* 次状態 busy を設定 */
     nmem[0].flag  = 1;
     nmem[0].ln    = term_num;
     nmem[0].state = busy;
 
-    nmem[1].flag = 0;
+    nmem[1].flag  = 0;
 }
