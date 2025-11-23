86c8253f60147cd016ca5ce799b29b53e90b31bd
e2358203-spec <e2358203@soka-u.jp>
2025-11-24 01:38:01 +0900

    Update task04.c

--- task04.c
+++ task04.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
 #include "header.h"
 
 extern MEM data[USERNUM];
 extern void output(int, int, int);
 
-void task04(int term_num, NMEM *nmem)
+void task04(int ln, NMEM *nmem)
 {
-    printf("\n 発信が許可されていません！\n");
-    output(connect, term_num, BUSYTONE);
+    /* 1. 全ての音を止める（存在するものだけ） */
+    output(disconnect, ln, SR);
+    output(disconnect, ln, DIALTONE);
+    output(disconnect, ln, RINGINGTONE);
+    output(disconnect, ln, RINGBACKTONE);
+    output(disconnect, ln, BUSYTONE);
 
-    nmem[0].flag  = 1;
-    nmem[0].ln    = term_num;
-    nmem[0].state = busy;
-
-    nmem[1].flag  = 0;
+    /* 2. 状態を idle に戻す */
+    nmem[0].flag = 1;
+    nmem[0].ln = ln;
+    nmem[0].state = idle;
 }
