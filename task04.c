666de1951afcb387dd249b2f1dda2a20a73a3d0d
e2358212 <e2358212@soka-u.jp>
2025-11-26 16:25:38 +0900

    Update task04.c

--- task04.c
+++ task04.c
@@ -4,16 +4,16 @@ extern MEM data[USERNUM];
 extern void output(int, int, int);
 
 void task04(int ln, NMEM *nmem)
-{
+{   
     /* 1. 全ての音を止める（存在するものだけ） */
-    output(disconnect, ln, SR);
-    output(disconnect, ln, DIALTONE);
-    output(disconnect, ln, RINGINGTONE);
-    output(disconnect, ln, RINGBACKTONE);
-    output(disconnect, ln, BUSYTONE);
+    // output(disconnect, ln, SR);
+    // output(disconnect, ln, DIALTONE);
+    // output(disconnect, ln, RINGINGTONE);
+    // output(disconnect, ln, RINGBACKTONE);
+    // output(disconnect, ln, BUSYTONE);
 
     /* 2. 状態を idle に戻す */
     nmem[0].flag = 1;
     nmem[0].ln = ln;
-    nmem[0].state = idle;
+    nmem[0].state = busy;
 }
